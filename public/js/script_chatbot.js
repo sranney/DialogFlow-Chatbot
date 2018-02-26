cb7abeac6176eb6f6639243c74cd151a3af2fe9a
Spencer <spencerranney@gmail.com>
2018-02-26 01:45:38 -0600

    fixing fumnctionality of choices

--- public/js/script_chatbot.js
+++ public/js/script_chatbot.js
@@ -304,7 +304,7 @@ socket.on("music",songData=>{
             spotify.setAttribute("src",this.parentNode.querySelector(".info").getAttribute("url"));
             document.querySelector(".user-section").style.display = "block";
             document.querySelector(".chatty-section").style.display = "none";
-            document.removeChild(swiperContainer);
+            document.querySelector("body").removeChild(".swiper-container");
         });
     })
 
